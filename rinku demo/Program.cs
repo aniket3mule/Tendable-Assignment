b982d4c53095dca92d247f00cf609d6dcf4772c2
Harshal Kadam <harshalkadam07@gmail.com>
2022-02-06 10:47:53 -0800

    master branch clean

--- rinku demo/Program.cs
+++ rinku demo/Program.cs
@@ -7,26 +7,8 @@ namespace rinku_demo
     {
         public static void Main()
         {
-            city obj = new city();
-            obj.Add(1,2,3);
         }
 
-    }
-    class city
-    {
-        public void Add(int a, int b)// function overloding
-        {
-            
-            int c = a + b;
-
-            Console.WriteLine("Add is {0}", c);
-        }
-        public void Add(int p, int b, int a)
-        {
-           
-            int m = a + b + p;
-            Console.WriteLine("Add is {0}", m);
-        }
     }
 }
 
