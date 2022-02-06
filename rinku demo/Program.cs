b1f7b449f6e3717a89633ad54beea410c33da2b1
Harshal Kadam <harshalkadam07@gmail.com>
2022-02-06 09:22:56 -0800

    Add project files.

---
+++ rinku demo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace rinku_demo
+{
+
+    class Program
+    {
+        public static void Main()
+        {
+            city obj = new city();
+            obj.Add(1,2,3);
+        }
+
+    }
+    class city
+    {
+        public void Add(int a, int b)// function overloding
+        {
+            
+            int c = a + b;
+
+            Console.WriteLine("Add is {0}", c);
+        }
+        public void Add(int z, int b, int a)
+        {
+           
+            int m = a + b + z;
+            Console.WriteLine("Add is {0}", m);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
